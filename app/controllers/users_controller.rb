5efbc667b2436fed7a42da00bca0b19b52fbcd72
toshiaki.atsumi <toshiaki.atsumi@techacademy.jp>
2019-05-16 23:02:17 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -19,6 +21,16 @@ class UsersController < ApplicationController
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
+    end
+  end
+  
+  include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
     end
   end
   def user_params
